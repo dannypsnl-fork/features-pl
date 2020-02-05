3cba9f8594ff927bd81353f7d10ef243f1c7d82c
dannypsnl <dannypsnl@gmail.com>
2020-02-05 21:08:44 +0800

    weak mark should apply on variable, not type

--- weak-mark/weak-mark.rkt
+++ weak-mark/weak-mark.rkt
@@ -1,42 +1,65 @@
 #lang typed/racket
 
-(struct TraitType (
-  ; fields
+(struct None ())
+(struct (a) Some ([v : a]))
+(define-type (Option a) (U None (Some a)))
+
+(struct type (
   [name : String]
   )
-  ; struct option
-  ; #:transparent option is important here because we want to compare TraitType directly 
-  #:transparent)
-(struct MarkableType (
-  ; fields
-  [name : String]
-  [mark : (Listof TraitType)]
+  #:transparent
   )
-  ; struct option
+(struct var (
+  [typ : type]
+  [tags : (Listof type)]
+  )
+  ; options
   #:mutable)
-
-(: new-type (-> String MarkableType))
-(define (new-type name)
-  (MarkableType name '()))
-(: type/add-tag (-> MarkableType TraitType Void))
-(define (type/add-tag self tag)
-  (set-MarkableType-mark! self
-    ; simply remove duplicates, performance is not the point at here
-    (remove-duplicates (list* tag (MarkableType-mark self)))
+(struct func-type (
+  [arg : type]
+  [arg-tag : (Listof type)]
+  [ret : type]
+  [tag-after-eval : (Option (-> var Void))]
   ))
-(: type/remove-tag (-> MarkableType TraitType Void))
-(define (type/remove-tag self tag)
-  (set-MarkableType-mark! self (remove tag (MarkableType-mark self))))
 
-;;; prepare environment
-(define env (make-hash))
+(: var/add-tag : var type -> Void)
+(define (var/add-tag self tag)
+  (set-var-tags! self
+    ; performance is not the point here
+    (remove-duplicates (list* tag (var-tags self)))
+  ))
 
-(define List (new-type "List")) ; (type List)
-(hash-set! env "x" List) ; (var x : List)
-; (func sort : List -> (Tag List sorted))
-; (sort x)
-; (var e : int)
-; (push-back x e)
-; (binary-search x) ; should report error
-; (sort x)
-; (binary-search x) ; should be fine
+(: application : func-type var -> Symbol)
+(define (application ft v)
+  ;; check type of arg and v-type are the same
+  (if (equal? (func-type-arg ft) (var-typ v))
+    (if (equal? (list->set (func-type-arg-tag ft)) (list->set (var-tags v)))
+       (let ()
+         (define eval (func-type-tag-after-eval ft))
+         (cond
+           [(Some? eval) ((Some-v eval) v)]) ; if some eval to do, apply it
+       'ok)
+       ; notice the incomplete part
+       ; we actually have to remove tags if no arg-tag requirement
+       ; but for this little example this is not so important
+       'err)
+    'err)
+  )
+
+(define List (type "List")) ; (type List)
+(define int (type "int")) ; (type int)
+(define void (type "void")) ; (type Void)
+(define sorted (type "sorted")) ; (type sorted)
+(define x (var List '())) ; (var x : List)
+(: tag-sorted : var -> Void)
+(define tag-sorted (lambda (arg-var) (var/add-tag arg-var sorted)))
+(define sort (func-type List '() void (Some tag-sorted))) ; (func sort : (x:List) -> Void, after: tag x sorted)
+(printf "(sort x):\n")
+(application sort x) ; (sort x)
+(define binary-search (func-type List (list sorted) int (None))) ; (func binary-search : (x:List with [sorted]) -> int, after: do-nothing)
+(printf "(binary-search x):\n")
+(application binary-search x) ; (binary-search x), should be fine
+(define e (var int '())) ; (var e : int)
+(define y (var List '())) ; (var y : List)
+(printf "(binary-search y):\n")
+(application binary-search y) ; (binary-search y), should report error
